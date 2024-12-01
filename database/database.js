b8d324142a63312c952768028ae1a43e4b38bac0
cyvo1912 <20212853@aloe.ulima.edu.pe>
2024-12-01 16:49:13 -0500

    cambio para el database

--- database/database.js
+++ database/database.js
@@ -1,8 +1,7 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
-dotenv.config(); // Cargar el archivo .env primero
 
-import { Sequelize } from 'sequelize';
+dotenv.config();
 
 export const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
